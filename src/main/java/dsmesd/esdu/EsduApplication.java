343e92e2e3f7f6deede19153aa4515d5bd21e885
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-25 19:03:00 +0900

    Add project base

---
+++ src/main/java/dsmesd/esdu/EsduApplication.java
@@ -0,0 +1,13 @@
+package dsmesd.esdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsmesd/esdu/EsduApplicationTests.java
@@ -0,0 +1,13 @@
+package dsmesd.esdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
