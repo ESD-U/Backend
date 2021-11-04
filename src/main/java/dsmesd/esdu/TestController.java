c2c00854d0a382aba830abef075bd8ef5b81d5c2
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-04 09:23:43 +0900

    Create environment test

--- src/main/java/dsmesd/esdu/TestController.java
+++ src/main/java/dsmesd/esdu/TestController.java
@@ -1,14 +1,18 @@
 package dsmesd.esdu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${envrionment.test.message}")
+    private String message;
+
     @GetMapping("/test")
     public String deployTest() {
-        return "deploy success";
+        return "deploy success" + message;
     }
 
 }
