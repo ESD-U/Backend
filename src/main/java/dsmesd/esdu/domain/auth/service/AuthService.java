39b6431609edce57024bc0f52a54595028285e98
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-07 23:20:35 +0900

    (#4) Add auth service

--- src/main/java/dsmesd/esdu/domain/auth/service/AuthService.java
+++ src/main/java/dsmesd/esdu/domain/auth/service/AuthService.java
@@ -1,18 +1,16 @@
 package dsmesd.esdu.domain.auth.service;
 
-import dsmesd.esdu.domain.auth.dto.request.LoginRequest;
 import dsmesd.esdu.domain.auth.dto.response.LoginResponse;
+import dsmesd.esdu.domain.auth.dto.response.TokenRefreshResponse;
 import dsmesd.esdu.domain.auth.entity.User;
 import dsmesd.esdu.domain.auth.exception.PasswordNotMatchedException;
 import dsmesd.esdu.domain.auth.exception.UserNotFoundException;
 import dsmesd.esdu.domain.auth.repository.UserRepository;
+import dsmesd.esdu.global.error.exception.InvalidTokenException;
 import dsmesd.esdu.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import javax.validation.Valid;
 
 @Service
 @RequiredArgsConstructor
@@ -22,17 +20,26 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
-        User user = userRepository.findById(request.getId())
+    public LoginResponse login(String id, String password) {
+        User user = userRepository.findById(id)
                 .orElseThrow(UserNotFoundException::new);
 
-        if (!passwordEncoder.matches(request.getPassword(),user.getPassword()))
+        if (!passwordEncoder.matches(password,user.getPassword()))
             throw new PasswordNotMatchedException();
 
         String accessToken = jwtTokenProvider.generateAccessToken(user.getId());
         String refreshToken = jwtTokenProvider.generateRefreshToken(user.getId());
 
         return new LoginResponse(accessToken, refreshToken);
+    }
+
+    public TokenRefreshResponse tokenRefresh(String refreshToken) {
+        if (!jwtTokenProvider.validateRefreshToken(refreshToken)) {
+            throw new InvalidTokenException();
+        }
+        String id = jwtTokenProvider.getId(refreshToken);
+        String accessToken = jwtTokenProvider.generateAccessToken(id);
+        return new TokenRefreshResponse(accessToken);
     }
 
 }
