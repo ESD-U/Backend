68356190fc5c135c2f821012569edafdd24107b1
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-07 23:20:45 +0900

    (#4) Add auth controller

--- src/main/java/dsmesd/esdu/domain/auth/controller/AuthController.java
+++ src/main/java/dsmesd/esdu/domain/auth/controller/AuthController.java
@@ -1,7 +1,9 @@
 package dsmesd.esdu.domain.auth.controller;
 
 import dsmesd.esdu.domain.auth.dto.request.LoginRequest;
+import dsmesd.esdu.domain.auth.dto.request.TokenRefreshRequest;
 import dsmesd.esdu.domain.auth.dto.response.LoginResponse;
+import dsmesd.esdu.domain.auth.dto.response.TokenRefreshResponse;
 import dsmesd.esdu.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +20,12 @@ public class AuthController {
 
     @PostMapping("/login")
     public LoginResponse login(@Valid @RequestBody LoginRequest request) {
-        return authService.login(request);
+        return authService.login(request.getId(), request.getPassword());
+    }
+
+    @PostMapping("/token-refresh")
+    public TokenRefreshResponse tokenRefresh(@Valid @RequestBody TokenRefreshRequest request) {
+        return authService.tokenRefresh(request.getRefreshToken());
     }
 
 }
