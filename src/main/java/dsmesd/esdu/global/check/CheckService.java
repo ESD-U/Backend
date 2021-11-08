84caf9f2b0fbd3c660f6d16fafe11b1113485122
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-08 16:58:54 +0900

    (#7) Add check service

---
+++ src/main/java/dsmesd/esdu/global/check/CheckService.java
@@ -0,0 +1,19 @@
+package dsmesd.esdu.global.check;
+
+import dsmesd.esdu.global.error.exception.InvalidSecretKeyException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckService {
+
+    @Value("${auth.request.secret}")
+    private String secretKey;
+
+    public void checkSecretKey(String secretKey) {
+        if (!this.secretKey.equals(secretKey)) {
+            throw new InvalidSecretKeyException();
+        }
+    }
+
+}
