5545f8dacf18a07a2347baf8326c4e393bc5d4ad
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-08 18:48:44 +0900

    (#8) Add light dto

---
+++ src/main/java/dsmesd/esdu/domain/light/dto/LightResponse.java
@@ -0,0 +1,12 @@
+package dsmesd.esdu.domain.light.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LightResponse {
+
+    private boolean info;
+
+}
